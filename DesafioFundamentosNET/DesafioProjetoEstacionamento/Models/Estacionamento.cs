f22a3ffce10aca9c1d72ef551c81de4a655ceb98
Richard <147954178+Ric-dll@users.noreply.github.com>
2024-01-01 17:34:11 -0300

    Add files via upload

---
+++ DesafioFundamentosNET/DesafioProjetoEstacionamento/Models/Estacionamento.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioProjetoEstacionamento.Models
+{
+    public class Estacionamento{ 
+        //Criando dois atributos privados para preço e a lista de veículos
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+        
+        //Construtor que contém dois parâmetros
+        public Estacionamento(decimal parPrecoInicial, decimal parPrecoPorHora){
+            this.precoInicial = parPrecoInicial;
+            this.precoPorHora = parPrecoPorHora;
+        }
+
+        //Métodos do programa
+        public void AdicionarVeiculo(){
+            //Recebendo e adicionando na lista
+            Console.WriteLine("Digite a Placa do Veículo para estacionar: ");
+            veiculos.Add(Console.ReadLine());
+            Console.WriteLine($"Veículo adicionado com Sucesso!");
+        }
+
+        public void RemoverVeiculo(){
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
+            // *IMPLEMENTE AQUI*
+            Console.WriteLine("Digite a Placa do Veículo para Remove-lô: ");
+            string placa = Console.ReadLine(); //Variável placa inicializada com valor dado pelo usuário
+
+            //Verificando se o veículo existe...
+            //A função Any retorna true se pelo menos um elemento na coleção satisfizer a condição especificada...
+            //Não é nesessário declarar de forma antecipada a variável item nesse contexto de lambda
+            //Item => item.ToUpper() significa que a variável item vai receber cada placa toando-a maiuscula e comparando com a placa digitada
+            if(veiculos.Any(item => item.ToUpper() == placa.ToUpper())){//Se sim
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu no local: ");
+
+                //Recebe as horas e converte para inteiro para realizar o calculo do valor de pagamento
+                int horas = Convert.ToUInt16(Console.ReadLine());
+
+                //Remove a placa digitada da lista de veículos
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O Veículo de placa: {placa} foi removido, Valor total a ser pago: {precoInicial + precoPorHora * horas:C}");
+            }else{
+                Console.WriteLine($"Veículo não encontrado. Verifique se a Placa foi digitada corretamente, Placa: {placa}");
+            }
+        }
+    
+        public void ListarVeiculosEstacionados(){
+            //Verifica se há veículos no estacionamento
+            if(veiculos.Any()){
+                Console.WriteLine("Os veículos estacionados são: \n");
+                for(int i=0; i<veiculos.Count; i++){
+                    Console.WriteLine($"[{i+1}] - {veiculos[i]}");
+                }
+            }else{
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ DesafioFundamentosNET/DesafioProjetoEstacionamento/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+using DesafioProjetoEstacionamento.Models;
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//Definição de variáveis necessárias
+
+bool Menu = true; //Define como verdadeiro para inicio quando for chamado no laço
+String opcaoMenu = string.Empty; //Define a string opcaoMenu com um valor vazio
+
+//Fim da definição definição de variáveis necessárias
+
+
+Console.WriteLine("_______[ Controle de Estacionamento ]_______");
+
+    Console.WriteLine("Para iniciar, defina um preço inicial: ");
+    decimal definicaoPrecoInicial = Convert.ToDecimal(Console.ReadLine());
+    /*Aqui usamos a função Convert.ToDecimal antes de usar a função ReadLine, pois como a função
+    recebe sempre uma String, ela será formatada para tipo decimal*/  
+
+    Console.WriteLine("Agora, defina o preço por hora: ");
+    decimal definicaoPrecoPorHora = Convert.ToDecimal(Console.ReadLine());
+    
+    //precoInicial e precoPorHora recebe a conversão para decimal do que o usuário digitar
+
+
+//Agora Instanciando a classe Estacionamento, com os valores obtidos anteriormente
+Estacionamento estacionamento = new Estacionamento(definicaoPrecoInicial, definicaoPrecoPorHora);
+
+while(Menu){
+    Console.Clear();//Limpa o que já foi exibido
+
+    //Menu de Opções
+    Console.WriteLine("___[ Controle de Estacionamento ]___");
+    Console.WriteLine("\n1. Cadastrar Veículo" + "\n2. Remover Veículo" + "\n3. Listar Veículos" + "\n4. Sair do programa \n\nOpção: ");
+
+    //De forma direta, espera a opção de entrada do usuário
+    switch(Console.ReadLine()){
+        case "1":
+            //Chama a função Para adicionar o veículo
+            estacionamento.AdicionarVeiculo();
+        break;
+
+        case "2":
+            //Chama a função Para Remover o veículo
+            estacionamento.RemoverVeiculo();
+        break;
+
+        case "3":
+            //Chama a função Para Listar Todos os veículos
+            estacionamento.ListarVeiculosEstacionados();
+        break;
+
+        case "4":
+            //Define menu como falso para encerrar o loop
+            Menu = false;
+        break;
+
+        default:
+            Console.WriteLine("Opção Inválida, Tente Novamente!...");
+        break;
+    }
+    Console.WriteLine($"Pressione Qualquer tecla para continuar...{Console.ReadLine()}");
+}
+
+Console.WriteLine("Programa Encerrado...");
